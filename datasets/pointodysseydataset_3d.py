74f54783106b7873f6cfa304938ea8a051d81c28
aharley <harley.adam@gmail.com>
2023-09-16 15:27:44 -0700

    made pod3d match pod2d better

--- datasets/pointodysseydataset_3d.py
+++ datasets/pointodysseydataset_3d.py
@@ -26,6 +26,7 @@ class PointOdysseyDataset(torch.utils.data.Dataset):
                  use_augs=False,
                  S=8,
                  N=32,
+                 strides=[1],
                  quick=False,
                  verbose=False,
     ):
@@ -75,7 +76,7 @@ class PointOdysseyDataset(torch.utils.data.Dataset):
                 if verbose: 
                     print('seq', seq)
                     
-                for stride in [1]:
+                for stride in strides:
                     for ii in range(0,len(os.listdir(rgb_path))-self.S*stride+1, 4):
                         full_idx = ii + np.arange(self.S)*stride
                         self.rgb_paths.append([os.path.join(seq, 'rgbs', 'rgb_%05d.jpg' % idx) for idx in full_idx])
@@ -97,51 +98,52 @@ class PointOdysseyDataset(torch.utils.data.Dataset):
         sample = None
         gotit = False
 
-
         rgb_paths = self.rgb_paths[index]
         depth_paths = self.depth_paths[index]
         full_idx = self.full_idxs[index]
         annotations_path = self.annotation_paths[index]
         annotations = np.load(annotations_path, allow_pickle=True)
-        # print(annotations.files)
-        # print('annotations_path', annotations_path)
-        # print('full_idx', full_idx)
-        # print('raw trajs_2d', annotations['trajs_2d'].shape)
-        # print('raw trajs_3d', annotations['trajs_3d'].shape)
-        
         trajs_2d = annotations['trajs_2d'][full_idx].astype(np.float32)
         visibs = annotations['visibilities'][full_idx].astype(np.float32)
         valids = (visibs<2).astype(np.float32)
         visibs = (visibs==1).astype(np.float32)
-
         trajs_world = annotations['trajs_3d'][full_idx].astype(np.float32)
         pix_T_cams = annotations['intrinsics'][full_idx].astype(np.float32)
         cams_T_world = annotations['extrinsics'][full_idx].astype(np.float32)
+
+        # ensure that the point is good in frame0
+        vis_and_val = valids * visibs
+        vis0 = vis_and_val[0] > 0
+        trajs_2d = trajs_2d[:,vis0]
+        trajs_world = trajs_world[:,vis0]
+        visibs = visibs[:,vis0]
+        valids = valids[:,vis0]
 
         S,N,D = trajs_2d.shape
         assert(D==2)
         assert(S==self.S)
         
+        if N < self.N//2:
+            print('returning before cropping: N=%d; need at least N=%d' % (N, self.N//2))
+            return None, False
+        
         trajs_cam = utils.geom.apply_4x4_py(cams_T_world, trajs_world)
         trajs_pix = utils.geom.apply_pix_T_cam_py(pix_T_cams, trajs_cam)
 
-        valids_xy = np.ones_like(trajs_2d)
-
         # get rid of infs and nans
+        valids_xy = np.ones_like(trajs_2d)
         inf_idx = np.where(np.isinf(trajs_2d))
         trajs_2d[inf_idx] = 0
         valids_xy[inf_idx] = 0
         nan_idx = np.where(np.isnan(trajs_2d))
         trajs_2d[nan_idx] = 0
         valids_xy[nan_idx] = 0
-
         inv_idx = np.where(np.sum(valids_xy, axis=2)<2) # S,N
         visibs[inv_idx] = 0
         valids[inv_idx] = 0
 
         rgbs = []
         for rgb_path in rgb_paths:
-            # rgbs.append(cv2.imread(rgb_path))
             with Image.open(rgb_path) as im:
                 rgbs.append(np.array(im)[:, :, :3])
 
@@ -150,9 +152,6 @@ class PointOdysseyDataset(torch.utils.data.Dataset):
             depth16 = cv2.imread(depth_path, cv2.IMREAD_ANYDEPTH)
             depth = depth16.astype(np.float32) / 65535.0 * 1000.0
             depths.append(depth)
-
-        H,W,C = rgbs[0].shape
-        assert(C==3)
 
         H,W,C = rgbs[0].shape
         assert(C==3)
@@ -167,17 +166,27 @@ class PointOdysseyDataset(torch.utils.data.Dataset):
 
             # when a point moves far oob, don't supervise with it
             very_oob_inds = np.logical_or(
-                np.logical_or(trajs_2d[si,:,0] < -32, trajs_2d[si,:,0] > W+32),
-                np.logical_or(trajs_2d[si,:,1] < -32, trajs_2d[si,:,1] > H+32))
+                np.logical_or(trajs_2d[si,:,0] < -64, trajs_2d[si,:,0] > W+64),
+                np.logical_or(trajs_2d[si,:,1] < -64, trajs_2d[si,:,1] > H+64))
             valids[si,very_oob_inds] = 0
 
-        # ensure that the point is valid at frame0
-        val0 = valids[0] > 0
-        trajs_2d = trajs_2d[:,val0]
-        trajs_cam = trajs_cam[:,val0]
-        trajs_pix = trajs_pix[:,val0]
-        visibs = visibs[:,val0]
-        valids = valids[:,val0]
+        # ensure that the point is good in frame0
+        vis_and_val = valids * visibs
+        vis0 = vis_and_val[0] > 0
+        trajs_2d = trajs_2d[:,vis0]
+        trajs_cam = trajs_cam[:,vis0]
+        trajs_pix = trajs_pix[:,vis0]
+        visibs = visibs[:,vis0]
+        valids = valids[:,vis0]
+
+        # ensure that the point is good in frame1
+        vis_and_val = valids * visibs
+        vis1 = vis_and_val[1] > 0
+        trajs_2d = trajs_2d[:,vis1]
+        trajs_cam = trajs_cam[:,vis1]
+        trajs_pix = trajs_pix[:,vis1]
+        visibs = visibs[:,vis1]
+        valids = valids[:,vis1]
 
         # ensure that the point is visible at frame0
         vis0 = visibs[0] > 0
@@ -187,18 +196,18 @@ class PointOdysseyDataset(torch.utils.data.Dataset):
         visibs = visibs[:,vis0]
         valids = valids[:,vis0]
 
-        # ensure that the point is visible in at least sqrt(S) frames
-        vis_ok = np.sum(visibs, axis=0) >= max(np.sqrt(S),2)
-        trajs_2d = trajs_2d[:,vis_ok]
-        trajs_cam = trajs_cam[:,vis_ok]
-        trajs_pix = trajs_pix[:,vis_ok]
-        visibs = visibs[:,vis_ok]
-        valids = valids[:,vis_ok]
+        # ensure that the point is good in at least sqrt(S) frames
+        val_ok = np.sum(valids, axis=0) >= max(np.sqrt(S),2)
+        trajs_2d = trajs_2d[:,val_ok]
+        trajs_cam = trajs_cam[:,val_ok]
+        trajs_pix = trajs_pix[:,val_ok]
+        visibs = visibs[:,val_ok]
+        valids = valids[:,val_ok]
         
         N = trajs_2d.shape[1]
         
-        if N==0:
-            print('N=%d' % (N))
+        if N < self.N//2:
+            # print('N=%d' % (N))
             return None, False
         
         if N < self.N:
@@ -213,21 +222,21 @@ class PointOdysseyDataset(torch.utils.data.Dataset):
             trajs_pix = trajs_pix[:,inds]
             visibs = visibs[:,inds]
             valids = valids[:,inds]
+
+        # clamp so that the trajectories don't get too crazy
+        trajs_2d = np.minimum(np.maximum(trajs_2d, np.array([-64,-64])), np.array([W+64, H+64])) # S,N,2
+        trajs_pix = np.minimum(np.maximum(trajs_pix, np.array([-64,-64])), np.array([W+64, H+64])) # S,N,2
             
         N = trajs_2d.shape[1]
         N_ = min(N, self.N)
         inds = np.random.choice(N, N_, replace=False)
 
-        # clamp so that the trajectories don't get too crazy
-        trajs_2d = np.minimum(np.maximum(trajs_2d, np.array([-32,-32])), np.array([W+31, H+31])) # S,N,2
-        trajs_pix = np.minimum(np.maximum(trajs_pix, np.array([-32,-32])), np.array([W+31, H+31])) # S,N,2
-        
+        # prep for batching, by fixing N
         trajs_2d_full = np.zeros((self.S, self.N, 2)).astype(np.float32)
         trajs_cam_full = np.zeros((self.S, self.N, 3)).astype(np.float32)
         trajs_pix_full = np.zeros((self.S, self.N, 2)).astype(np.float32)
         visibs_full = np.zeros((self.S, self.N)).astype(np.float32)
         valids_full = np.zeros((self.S, self.N)).astype(np.float32)
-
         trajs_2d_full[:,:N_] = trajs_2d[:,inds]
         trajs_cam_full[:,:N_] = trajs_cam[:,inds]
         trajs_pix_full[:,:N_] = trajs_pix[:,inds]
@@ -263,7 +272,10 @@ class PointOdysseyDataset(torch.utils.data.Dataset):
             # return a fake sample, so we can still collate
             sample = {
                 'rgbs': torch.zeros((self.S, 3, 540, 960)),
-                'trajs': torch.zeros((self.S, self.N, 2)),
+                'depths': torch.zeros((self.S, 1, 540, 960)),
+                'trajs_2d': torch.zeros((self.S, self.N, 2)),
+                'trajs_cam': torch.zeros((self.S, self.N, 3)),
+                'trajs_pix': torch.zeros((self.S, self.N, 2)),
                 'visibs': torch.zeros((self.S, self.N)),
                 'valids': torch.zeros((self.S, self.N)),
             }
