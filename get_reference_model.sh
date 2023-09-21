76b54bf3e1eee23aa051cf3f0817728ed4a28829
David Li <dli7319@gmail.com>
2023-09-21 09:59:14 -0400

    Update get_reference_model.sh
    Directly wget to reference_model.tar.gz

--- get_reference_model.sh
+++ get_reference_model.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 echo "downloading the model from dropbox..."
-wget https://www.dropbox.com/scl/fi/czdlt2zc2ji2b7zd0pvoe/reference_model.tar.gz?rlkey=56ebq4g5dk01kyq8kuismev14
-
-echo "cleaning the filename..."
-mv reference_model.tar.gz?rlkey=ec9igxl3i57llwxubb294syf9 reference_model.tar.gz
+wget https://www.dropbox.com/scl/fi/czdlt2zc2ji2b7zd0pvoe/reference_model.tar.gz?rlkey=56ebq4g5dk01kyq8kuismev14 \
+  -O reference_model.tar.gz
 
 echo "extracting from tar..."
 tar -xvf reference_model.tar.gz
